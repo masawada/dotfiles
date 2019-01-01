775b4de0138c9dd22aeb7a5de241e322013df16a
Masayoshi Wada <developer@andantesoftware.com>
2019-01-01 23:17:25 +0900

    define include_role

--- lib/recipe_helper.rb
+++ lib/recipe_helper.rb
@@ -3,4 +3,9 @@ MItamae::RecipeContext.class_eval do
     root_dir = File.expand_path('../..', __FILE__)
     include_recipe File.join(root_dir, 'cookbooks', name, 'default')
   end
+
+  def include_role(name)
+    root_dir = File.expand_path('../..', __FILE__)
+    include_recipe File.join(root_dir, 'roles', name, 'default')
+  end
 end
