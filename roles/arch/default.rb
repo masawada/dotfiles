aa69a2e50992c303dbb345366efa457bc77c3567
Masayoshi Wada <developer@andantesoftware.com>
2019-01-01 23:17:37 +0900

    include all cookbooks

--- roles/arch/default.rb
+++ roles/arch/default.rb
@@ -1,1 +1,16 @@
 include_cookbook 'functions'
+
+include_cookbook 'yay'
+include_cookbook 'vim'
+include_cookbook 'zsh'
+include_cookbook 'tmux'
+
+include_cookbook 'i3'
+include_cookbook 'urxvt'
+include_cookbook 'fonts'
+include_cookbook 'ibus-skk'
+
+include_cookbook 'docker'
+
+include_cookbook 'google-chrome'
+include_cookbook 'slack'
