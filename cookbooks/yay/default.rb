bbdb6f4abd4a14b5ca2e314e0dac396414cd8d23
Masayoshi Wada <developer@andantesoftware.com>
2019-01-02 19:32:50 +0900

    use --noprovides option to avoid looking for matching providers

--- cookbooks/yay/default.rb
+++ cookbooks/yay/default.rb
@@ -12,13 +12,13 @@ execute "tar zxvf #{archive}" do
   not_if 'command -v yay'
   cwd '/tmp'
 end
-execute "#{extracted_bin} --noconfirm yay-bin" do
+execute "#{extracted_bin} --noconfirm --noprovides yay-bin" do
   not_if 'command -v yay'
 end
 
 define :yay do
   name = params[:name].shellescape
-  execute "yay --noconfirm #{name}" do
+  execute "yay --noconfirm --noprovides #{name}" do
     not_if "yay -Q #{name} || yay -Qg #{name}"
     if ENV['SUDO_USER']
       user ENV['SUDO_USER']
