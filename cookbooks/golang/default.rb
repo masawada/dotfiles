327bc041570f325242d4ac064e5aaa46673f3891
Masayoshi Wada <developer@andantesoftware.com>
2019-01-03 12:53:12 +0900

    install gore and recommend packages

--- cookbooks/golang/default.rb
+++ cookbooks/golang/default.rb
@@ -1,1 +1,19 @@
 package 'go'
+
+gopath = "#{ENV['HOME']}/.go"
+directory gopath do
+  not_if File.exist?(gopath)
+  user node[:user]
+end
+
+%w[
+  github.com/motemen/gore
+  github.com/nsf/gocode
+  github.com/k0kubun/pp
+  golang.org/x/tools/cmd/godoc
+].each do |repo|
+  execute "GOPATH=#{gopath} go get -u #{repo}" do
+    not_if File.exist?(File.join(gopath, 'src', repo))
+    user node[:user]
+  end
+end
