346158060d0d98fb6def25ef7047e31e91e1accf
Masayoshi Wada <developer@andantesoftware.com>
2019-01-03 12:38:31 +0900

    define uname constant

--- lib/recipe.rb
+++ lib/recipe.rb
@@ -1,6 +1,7 @@
 include_recipe 'recipe_helper'
 
 node.reverse_merge!(
+  os: run_command('uname').stdout.strip.downcase,
   user: ENV['SUDO_USER'] || ENV['USER'],
   dot_dir: File.expand_path('../..', __FILE__),
 )
