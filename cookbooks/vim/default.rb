622b812a4e8c56273b8c57343a2cb38ef70d4211
Masayoshi Wada <developer@andantesoftware.com>
2019-01-14 02:13:35 +0900

    fetch plug.vim

--- cookbooks/vim/default.rb
+++ cookbooks/vim/default.rb
@@ -6,3 +6,13 @@ when 'darwin'
     options '--with-lua'
   end
 end
+
+dotfile 'vimrc'
+dotfile 'vim'
+
+
+plug_vim_source = "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
+plug_vim_path = "#{node[:dot_dir]}/config/vim/autoload/plug.vim"
+execute "curl -fSL -o #{plug_vim_path} #{plug_vim_source}" do
+  not_if "test -f #{plug_vim_path}"
+end
