ed6806dca5b09f8a63aa5a1905b83489d7b2de0d
Masayoshi Wada <developer@andantesoftware.com>
2019-01-29 00:22:18 +0900

    add user to docker group

--- cookbooks/docker/default.rb
+++ cookbooks/docker/default.rb
@@ -1,2 +1,12 @@
 package 'docker'
 package 'docker-compose'
+
+group 'docker' do
+  not_if 'getent group docker'
+end
+
+user = node[:user]
+execute 'add user to docker group' do
+  not_if "getent group docker | grep #{user}"
+  command "usermod #{user} -aG docker"
+end
