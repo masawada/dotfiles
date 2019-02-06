0ce80a206e81cfa49416cfb196a8851cc8c4525c
Masayoshi Wada <developer@andantesoftware.com>
2019-02-06 19:51:43 +0900

    code for multi monitors

--- config/config/polybar/launch.sh
+++ config/config/polybar/launch.sh
@@ -4,4 +4,10 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-polybar -q top &
+if type xrandr > /dev/null; then
+  for m in $(xrandr --query | grep "\bconnected\b" | awk '{ print $1 }'); do
+    MONITOR=$m polybar -q top &
+  done
+else
+  MONITOR=eDP-1 polybar -q top &
+fi
