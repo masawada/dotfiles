6b16d00ea4629a8a6a6c3664adc981bd19169b49
Masayoshi Wada <developer@andantesoftware.com>
2020-06-22 00:04:41 +0900

    use hostname based config

--- config/config/polybar/launch.sh
+++ config/config/polybar/launch.sh
@@ -4,10 +4,13 @@ killall -q polybar
 
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
+CONFIG_PATH=$HOME/.config/polybar/config-$(hostname)
+[ ! -e $CONFIG_PATH ] && CONFIG_PATH=$HOME/.config/polybar/config
+
 if type xrandr > /dev/null; then
   for m in $(xrandr --query | grep "\bconnected\b" | awk '{ print $1 }'); do
-    MONITOR=$m polybar -q top &
+    MONITOR=$m polybar -q -c $CONFIG_PATH top &
   done
 else
-  MONITOR=eDP-1 polybar -q top &
+  MONITOR=eDP-1 polybar -q -c $CONFIG_PATH top &
 fi
