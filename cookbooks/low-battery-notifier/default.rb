b69940449d46385ad048de8cb37ff71cef915e89
Masayoshi Wada <developer@andantesoftware.com>
2020-06-22 09:58:50 +0900

    enable/start on a laptop device

--- cookbooks/low-battery-notifier/default.rb
+++ cookbooks/low-battery-notifier/default.rb
@@ -3,4 +3,5 @@ dotfile '.config/systemd/user/low-battery-notifier.service'
 
 user_service 'low-battery-notifier.timer' do
   action [:enable, :start]
+  only_if "test -e /sys/class/power_supply/BAT0"
 end
