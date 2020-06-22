1fc83392ceafdf8ab29f8de96d02507e3791472a
Masayoshi Wada <developer@andantesoftware.com>
2020-06-22 09:53:44 +0900

    create symlink to siji.bdf

--- cookbooks/fonts/default.rb
+++ cookbooks/fonts/default.rb
@@ -16,3 +16,9 @@ end
 ].each do |font|
   yay font
 end
+
+# workaround for freetype2
+# https://masawada.hatenablog.jp/entry/2019/05/07/114349
+link File.join(ENV['HOME'], '.local', 'share', 'fonts', 'siji.bdf') do
+  to "/usr/share/fonts/misc/siji.bdf"
+end
