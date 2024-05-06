3a4239954a6d2b5f73d0348a9850195bc2059162
Masayoshi Wada <developer@andantesoftware.com>
2024-05-06 18:23:14 +0900

    fix warnings

--- config/config/polybar/launch.sh
+++ config/config/polybar/launch.sh
@@ -5,12 +5,12 @@ killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
 CONFIG_PATH=$HOME/.config/polybar/config-$(cat /etc/hostname)
-[ ! -e $CONFIG_PATH ] && CONFIG_PATH=$HOME/.config/polybar/config
+[ ! -e "$CONFIG_PATH" ] && CONFIG_PATH="$HOME/.config/polybar/config"
 
 if type xrandr > /dev/null; then
   for m in $(xrandr --query | grep "\bconnected\b" | awk '{ print $1 }'); do
-    MONITOR=$m polybar -q -c $CONFIG_PATH top &
+    MONITOR=$m polybar -q -c "$CONFIG_PATH" top &
   done
 else
-  MONITOR=eDP-1 polybar -q -c $CONFIG_PATH top &
+  MONITOR=eDP-1 polybar -q -c "$CONFIG_PATH" top &
 fi
