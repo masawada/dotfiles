23e73c9433d0da6fa0b083cb07219c9c045d90e9
Masayoshi Wada <developer@andantesoftware.com>
2024-05-21 00:14:40 +0900

    export SSH_AUTH_SOCK

--- recipes/ssh-agent/main.sh
+++ recipes/ssh-agent/main.sh
@@ -4,4 +4,10 @@ dotfile "pam-environment"
 enable_user_systemd_service "ssh-agent"
 start_user_systemd_service "ssh-agent"
 
+if pgrep ssh-agent > /dev/null; then
+  ssh_agent_pid="$(pgrep ssh-agent)"
+  export SSH_AGENT_PID="$ssh_agent_pid"
+  export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/ssh-agent.socket"
+fi
+
 execute "ssh-add $HOME/.ssh/id_ed25519"
