5eb19fb12e4a87649a6db930c58bb1cc2cbeb6bc
Masayoshi Wada <developer@andantesoftware.com>
2024-05-22 13:56:40 +0900

    update sha256sum

--- recipes/ibus-skk/main.sh
+++ recipes/ibus-skk/main.sh
@@ -6,7 +6,7 @@ config_dir="$HOME/.config/ibus-skk"
 
 install_file_from_url \
   "https://raw.githubusercontent.com/tokuhirom/jawiki-kana-kanji-dict/master/SKK-JISYO.jawiki" \
-  "3712ba9839838b3a00b2d3405783dc059e6c67ce033374e64f788918f400a451" \
+  "38271b80fd34eb7244b26eba8d9d90458e0625c9c788e3b9e0da0c8ffe087167" \
   "$config_dir/SKK-JISYO.jawiki"
 
 execute "dconf write /desktop/ibus/engine/skk/dictionaries \"['file=$config_dir/user.dict,mode=readwrite,type=file','file=/usr/share/skk/SKK-JISYO.L,mode=readonly,type=file','file=$config_dir/SKK-JISYO.jawiki,mode=readonly,type=file,encoding=UTF-8']\""
