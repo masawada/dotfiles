3a0c6eb4233d1716763ba16f44ecb3f759bc21cc
Masayoshi Wada <developer@andantesoftware.com>
2024-05-25 03:16:40 +0900

    use wlogout and hyprlock

--- recipes/wayland/main.sh
+++ recipes/wayland/main.sh
@@ -1,6 +1,9 @@
 package "hyprland"
 dotfile "config/hypr/hyprland.conf"
 
+package "hyprlock"
+dotfile "config/hypr/hyprlock.conf"
+
 package "waybar"
 
 package "wofi"
@@ -9,3 +12,6 @@ dotfile "config/wofi/style.css"
 
 aur "hyprshot"
 create_directory "$HOME/Pictures"
+
+aur "wlogout"
+dotfile "config/wlogout/layout"
