67d74b14860f030b1cac44c620d0fb9aa19aadfa
Masayoshi Wada <developer@andantesoftware.com>
2024-05-25 15:27:31 +0900

    assign trackball sub-buttons for back/forward

--- recipes/wayland/main.sh
+++ recipes/wayland/main.sh
@@ -15,3 +15,14 @@ create_directory "$HOME/Pictures"
 
 aur "wlogout"
 dotfile "config/wlogout/layout"
+
+# configure mouse button
+system_file "etc/udev/hwdb.d/70-raspberry-pi-bluetooth-receiver.hwdb"
+execute_su "systemd-hwdb update"
+execute_su "udevadm trigger"
+
+aur "xremap-hypr-bin"
+dotfile "config/xremap/config.yml"
+add_user_to_group "$USER" "input"
+system_file "etc/modules-load.d/uinput.conf"
+system_file "etc/udev/rules.d/99-input.rules"
