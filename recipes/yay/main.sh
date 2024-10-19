ebd3e862e5e75265b716b1e647dde3e69941cbff
Masayoshi Wada <developer@andantesoftware.com>
2024-10-19 23:48:22 +0900

    upgrade yay

--- recipes/yay/main.sh
+++ recipes/yay/main.sh
@@ -1,6 +1,6 @@
-version="12.3.5"
+version="12.4.2"
 url="https://github.com/Jguer/yay/releases/download/v${version}/yay_${version}_x86_64.tar.gz"
-sha256_checksum="09b570e649aa7b111305ce1df9a83f0e88b06c191eeb8277aad2ac1ead2cbd6f"
+sha256_checksum="1ad3e5dbc410edc668eb9062bb6e56b90da818780581f06aa79dd696aa0b5ceb"
 extracted_dir="yay_${version}_x86_64"
 
 if ! type "yay" > /dev/null 2>&1; then
