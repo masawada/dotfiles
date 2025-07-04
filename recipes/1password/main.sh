a0bf8ef02ab092942adf108726f4615e9a50ec45
Masayoshi Wada <developer@andantesoftware.com>
2025-07-04 17:33:00 +0900

    add macos support for desktop application recipes

--- recipes/1password/main.sh
+++ recipes/1password/main.sh
@@ -1,1 +1,5 @@
-aur "1password"
+if is_macos; then
+  cask "1password"
+elif is_linux; then
+  aur "1password"
+fi
--- recipes/google-chrome/main.sh
+++ recipes/google-chrome/main.sh
@@ -1,2 +1,6 @@
-aur "google-chrome"
-dotfile "config/chrome-flags.conf"
+if is_macos; then
+  cask "google-chrome"
+elif is_linux; then
+  aur "google-chrome"
+  dotfile "config/chrome-flags.conf"
+fi
