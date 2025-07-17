acf0e629f7e7e7620d03b1085bc1457798b96957
Masayoshi Wada <developer@andantesoftware.com>
2025-07-18 01:34:02 +0900

    fix ntp recipe to use if statement for linux-only check

--- recipes/ntp/main.sh
+++ recipes/ntp/main.sh
@@ -1,8 +1,8 @@
 # This recipe is Linux-only
-is_linux || return
+if is_linux; then
+  package "ntp"
+  system_file "etc/ntp.conf"
 
-package "ntp"
-system_file "etc/ntp.conf"
-
-enable_systemd_service "ntpd"
-start_systemd_service "ntpd"
+  enable_systemd_service "ntpd"
+  start_systemd_service "ntpd"
+fi
