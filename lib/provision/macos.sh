6b8fe9fb51a6c52ffcc6cf36ac9ee4fe6a9626ee
Masayoshi Wada <developer@andantesoftware.com>
2025-07-21 00:56:13 +0900

    Move macos-defaults recipe to the end of provisioning

--- lib/provision/macos.sh
+++ lib/provision/macos.sh
@@ -3,7 +3,6 @@
 
 load_recipe "mas-cli"
 load_recipe "xcode"
-load_recipe "macos-defaults"
 load_recipe "macskk"
 load_recipe "sequel-ace"
 load_recipe "chatgpt"
@@ -12,3 +11,6 @@ load_recipe "raindropio"
 load_recipe "the-unarchiver"
 load_recipe "iterm2"
 load_recipe "karabiner-elements"
+
+# Load macos-defaults last to apply system preferences after all apps are installed
+load_recipe "macos-defaults"
