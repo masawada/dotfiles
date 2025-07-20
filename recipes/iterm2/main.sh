bc45144aeb538d58c083ed5244561e70ecc10021
Masayoshi Wada <developer@andantesoftware.com>
2025-07-20 18:22:50 +0900

    Configure iTerm2 to load preferences from custom folder

--- recipes/iterm2/main.sh
+++ recipes/iterm2/main.sh
@@ -1,3 +1,6 @@
 cask "iterm2"
 
 dotfile "iterm2"
+
+execute "defaults write com.googlecode.iterm2 LoadPrefsFromCustomFolder -bool true"
+execute "defaults write com.googlecode.iterm2 PrefsCustomFolder -string '$HOME/.iterm2'"
