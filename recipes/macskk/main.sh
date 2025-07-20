0b8e836e8136eccff0585f7490f2a7af12e43d62
Masayoshi Wada <developer@andantesoftware.com>
2025-07-20 17:21:15 +0900

    Add SKK-JISYO.L dictionary download to macSKK recipe

--- recipes/macskk/main.sh
+++ recipes/macskk/main.sh
@@ -7,6 +7,11 @@ home_file "Library/Containers/net.mtgto.inputmethod.macSKK/Data/Library/Preferen
 create_directory "$HOME/Library/Containers/net.mtgto.inputmethod.macSKK/Data/Documents/Dictionaries"
 
 install_file_from_url \
+  "https://raw.githubusercontent.com/skk-dev/dict/master/SKK-JISYO.L" \
+  "e896fc45e4185a0bc6216cf4e4be415cbc0128171d3ec42bc96c6f58f7a10f24" \
+  "$HOME/Library/Containers/net.mtgto.inputmethod.macSKK/Data/Documents/Dictionaries/SKK-JISYO.L"
+
+install_file_from_url \
   "https://raw.githubusercontent.com/tokuhirom/jawiki-kana-kanji-dict/master/SKK-JISYO.jawiki" \
   "4f2746b693604249a122af714ca99ad88aadab774457f0f302b5b274b4d60cb7" \
   "$HOME/Library/Containers/net.mtgto.inputmethod.macSKK/Data/Documents/Dictionaries/SKK-JISYO.jawiki"
